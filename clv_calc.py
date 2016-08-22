226d3454743b55cc8ca99952cdc2dc9d53f16790
jiacheng-yao <yao.jiacheng@rocket-internet.com>
2016-08-22 19:38:52 +0200

    minor changes, filtering old orders out of the transaction dataset

--- clv_calc.py
+++ clv_calc.py
@@ -13,11 +13,15 @@ is_summary_available = True
 input_file = "FD DE customer orders and revenue (1).xlsx"
 output_file = "summary_fd_de_customers.csv"
 
+plot_source = "fd"
+
 if is_summary_available is False:
     # transaction_data = pd.read_csv(input_file, sep=';')
     transaction_data = pd.read_excel(input_file, sep=';') # for FD dataset
 
-    summary = summary_data_from_transaction_data(transaction_data,
+    recent_transaction_data = transaction_data[transaction_data['order_date'] > "2015-07-31"]
+
+    summary = summary_data_from_transaction_data(recent_transaction_data,
                                                  'customer_id', 'order_date',
                                                  'revenue', observation_period_end='2016-08-03')
 
@@ -26,6 +30,8 @@ if is_summary_available is False:
     summary.to_csv(output_file, sep=';', encoding='utf-8')
 else:
     transaction_data = pd.read_csv(input_file, sep=';')
+
+    recent_transaction_data = transaction_data[transaction_data['order_date'] > "2015-07-31"]
 
     summary = pd.read_csv(output_file, sep=';')
 
@@ -40,28 +46,28 @@ bgf.fit(summary['frequency'],
 print bgf
 
 plot_frequency_recency_matrix(bgf)
-save("f_r_matrix_fd", ext="pdf", close=True, verbose=True)
+save("f_r_matrix_{}".format(plot_source), ext="pdf", close=True, verbose=True)
 
 plot_probability_alive_matrix(bgf)
-save("pr_alive_matrix_fd", ext="pdf", close=True, verbose=True)
+save("pr_alive_matrix_{}".format(plot_source), ext="pdf", close=True, verbose=True)
 
 plot_period_transactions(bgf)
-save("period_transactions_fd", ext="pdf", close=True, verbose=True)
+save("period_transactions_{}".format(plot_source), ext="pdf", close=True, verbose=True)
 
 id = '80_3811'
 days_since_birth = 365
 sp_trans = transaction_data.ix[transaction_data['customer_id'] == id]
 plot_history_alive(bgf, days_since_birth, sp_trans, 'order_date')
-save("history_alive_fd", ext="pdf", close=True, verbose=True)
+save("history_alive_{}".format(plot_source), ext="pdf", close=True, verbose=True)
 
 summary_cal_holdout = calibration_and_holdout_data(transaction_data, 'customer_id', 'order_date',
-                                                       calibration_period_end='2016-02-01',
+                                                       calibration_period_end='2016-05-01',
                                                        observation_period_end='2016-08-03')
 print summary_cal_holdout.head()
 
 bgf.fit(summary_cal_holdout['frequency_cal'], summary_cal_holdout['recency_cal'], summary_cal_holdout['T_cal'])
 plot_calibration_purchases_vs_holdout_purchases(bgf, summary_cal_holdout)
-save("cal_vs_holdout_fd", ext="pdf", close=True, verbose=True)
+save("cal_vs_holdout_{}".format(plot_source), ext="pdf", close=True, verbose=True)
 
 ggf = GammaGammaFitter(penalizer_coef = 0)
 ggf.fit(returning_customers_summary['frequency'],
